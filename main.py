cf5c62a0e543f758d8e361886bd958360384c584
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-13 16:35:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,14 @@
 import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
-from ydata_profiling import ProfileReport
 
 df_pl = pl.read_csv("cereal.csv")
 
-
 def summary():
     """EDA with Polars describe function to get mean, median, and standard deviation"""
-    print(df_pl.describe())
-
-def pdf_report_generator(df_pl):
-    profile = ProfileReport(df_pl, title="Summary Report")
-    profile.to_file("Summary_Report.html")
+    outcome = df_pl.describe()
+    summary_html = outcome.to_html("summary_statistics.html")
+    return outcome, summary_html
 
 def histogram():
     """Displays histogram with seaborn and matplotlib"""
@@ -22,6 +18,3 @@ def histogram():
     plt.title("Calories of Cereals (n=77)")
     plt.show()
     plt.savefig("Calories_of_Cereals.png")
-    print(df_pl)
-    pdf_report_generator(df_pl)
-    return df_pl
