2cef2edf74d7ae1a38f88ab82aec66efaa6514d6
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-13 18:30:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from ydata_profiling import ProfileReport
 
 df_pl = pl.read_csv("cereal.csv")
+pandas_df1 = df_pl.to_pandas()
 
 def summary():
     """EDA with Polars describe function to get mean, median, and standard deviation"""
@@ -17,7 +18,7 @@ def histogram():
     plt.title("Calories of Cereals (n=77)")
     plt.show()
     plt.savefig("Calories_of_Cereals.png")
-    pdf_report_generator(df_pl)
+    pdf_report_generator(pandas_df1)
 
 def pdf_report_generator(df):
     profile = ProfileReport(df, title="Summary Report")
