3716ff44d464aee0dd302f6925553781bfe4601c
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-13 16:06:44 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -9,14 +9,15 @@ from main import histogram
 def test_summary():
     """Test defined summary (or describe) function"""
     return summary()
-
+    assert df_pl["calories"]["mean"] == 106.88311688311688
+    assert df1["protein"]["max"] == 6.0
+    assert df1["fat"]["min"] == 0
 
 def test_histogram():
     """
     Test defined seeplot function to checkout a scatter plot
     """
     return histogram()
-
 
 if __name__ == "__main__":
     test_summary()
