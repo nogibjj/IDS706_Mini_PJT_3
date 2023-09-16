9c3427f6d249abe14474293aa8d3fc315de9930a
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-16 17:38:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from ydata_profiling import ProfileReport
 df_pl = pl.read_csv("cereal.csv")
 pandas_df1 = df_pl.to_pandas()
 
-def summary():
+def summary_descriptive_statistics():
     """EDA with Polars describe function to get mean, median, and standard deviation"""
     statistics = df_pl.describe()
     print(statistics)
     return statistics 
 
-def histogram():
+def histogram_plot_saved():
     """Displays histogram with seaborn and matplotlib"""
     sns.histplot(data=df_pl, x="calories", kde=False, bins=20)
     plt.title("Calories of Cereals (n=77)")
