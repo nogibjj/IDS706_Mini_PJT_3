9517a4fdb72e578231e5102a6c674cb1f071c7ce
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-16 17:37:11 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,19 +5,22 @@ Start Test
 import main 
 
 
-def test_summary():
-    new = main.summary()
-    """Test summary (or describe) function"""
-    assert new["calories"][2] == 106.88311688311688
-    assert new["protein"][-1] == 6.0
-    assert new["fat"][4] == 0.0
+def test_summary_descriptive_statistics():
+    """Test the summary function for descriptive statistics"""
+    statistics = main.summary()
+    
+    assert statistics["calories"]["mean"] == 106.88
+    assert statistics["protein"]["max"] == 6.0
+    assert statistics["fat"]["25%"] == 0.0
 
-def test_histogram():
-    """
-    Test histogram function set on main file
-    """
+def test_histogram_plot_saved():
+    """Test that the histogram function saves a histogram file"""
     main.histogram()
+    # check if the file 'Calories_of_Cereals.png" was created well 
+    import os 
+    assert os.path.isfile("Calories_of_Cereals.png")
 
 if __name__ == "__main__":
-    test_summary()
-    test_histogram()
+    # Run the tests 
+    test_summary_descriptive_statistics()
+    test_histogram_plot_saved()
