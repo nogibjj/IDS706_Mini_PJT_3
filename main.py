6f218273e6b8a61a67de793482ebe691f4d725b4
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-09-17 00:46:59 +0000

    update files

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 """Start main function"""
 
 import polars as pl
-import seaborn as sns
 import matplotlib.pyplot as plt
 from ydata_profiling import ProfileReport
 
-df_pl = pl.read_csv("cereal.csv")
-pandas_df = df_pl.to_pandas()
+df = pl.read_csv("cereal.csv")
 
-def summary():
+
+def summary(data):
     """EDA with Polars describe function to get mean, median, and standard deviation"""
-    print(df_pl.describe())
-    return df_pl.describe() 
+    print(data.describe())
+    return data.describe()
 
-def histogram():
+
+def histogram(data):
     """Displays histogram with seaborn and matplotlib"""
-    sns.histplot(data=df_pl, x="calories", kde=False, bins=20)
+    plt.hist(data["calories"], bins=20)
     plt.title("Calories of Cereals (n=77)")
+    plt.xlabel("Calories")
+    plt.ylabel("Frequency")
     plt.savefig("Calories_of_Cereals.png")
     plt.show()
-    pdf_report_generator(pandas_df)
+    pdf_report_generator(data)
 
-def pdf_report_generator(df):
-    profile = ProfileReport(df, title="Summary Report")
+
+def pdf_report_generator(data):
+    """Generate summary report"""
+    profile = ProfileReport(data, title="Summary Report")
     profile.to_file("Summary_Report.html")
+
+
+if __name__ == "__main__":
+    summary(df)
+    histogram(df)
--- test_main.py
+++ test_main.py
@@ -2,18 +2,20 @@
 Start Test
 """
 
-import main 
+import polars as pl
+from main import summary
 
 
 def test_summary():
-    """Test the summary function for descriptive statistics"""
-    new = main.summary()
+    """
+    test summary function in main.py
+    """
+    data = pl.read_csv("cereal.csv")
+    new = summary(data)
     assert new["calories"][2] == 106.88311688311688
     assert new["protein"][-1] == 6.0
     assert new["fat"][4] == 0.0
 
 
 if __name__ == "__main__":
-    # Run the tests 
     test_summary()
-    test_histogram()
